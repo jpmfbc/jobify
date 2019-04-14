669de74fbf9d6460017f169dc1894f9524d7c919
joão paulo <jpmfbc@gmail.com>
2019-04-14 11:02:03 -0300

    porta que o roda o site ficar dinamica e não permitir acesso a area admin sem autorizacao

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
+const port =  process.env.PORT || 3000
 
+app.use('/adim',(request,response,next) => {
+    if(request.hostname === "localhost"){
+        next()
+    }else{
+        response.send('Not allowed')
+    }
+})
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -82,7 +90,7 @@ const init = async () => {
 }
 init();
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.log('Não foi possivel iniciar servidor do jobify')
     } else {
